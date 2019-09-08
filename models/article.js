c929bfc854fff9633905ababc927c80d3855aab5
cramos <chloieeeramos@gmail.com>
2019-09-07 20:17:27 -0400

    updated article.js

---
+++ models/article.js
@@ -0,0 +1,37 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var ArticleSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  
+  link: {
+    type: String,
+    required: true
+  },
+  
+   summary: {
+      type: String,
+      required: true
+  },
+
+   note: [ {
+      type: Schema.Types.ObjectId,
+      ref: "Note"
+  }],
+      
+    saved: {
+        type: Boolean,
+        default: false
+   },
+  });
+    
+   
+var Article = mongoose.model("Article", ArticleSchema);
+    
+// Export the Article model
+
+module.exports = Article;
